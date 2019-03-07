34a6c797240fcdaa71d5b09a040bd922645c126a
TimotheeL <timothee.laurent@yahoo.fr>
2019-03-07 18:09:28 +0100

    Trimmed down example program to the bare minimum

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,8 +1,9 @@
-/* Fonctionnement de GLUt                       */
-/*                                              */
-/* Auteur: Nicolas JANEY                        */
-/* nicolas.janey@univ-fcomte.fr                 */
-/* Janvier 2019                                 */
+/* 
+	Racing game project - Main program
+	
+	2019
+	Nicolas Bouchard, Timothée Guy, Timothée Laurent
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,23 +12,11 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 
-/* Variables globales                           */
+/* Global variables */
+static int pMode = 1; 
 
-static float rx = 0.0F;            // Angle de rotation de la scene sur elle-meme autour de l'axe x
-static float sens = 1.0F;          // Sens de rotation de la scene sur elle-meme autour de l'axe x
-static float ry = 0.0F;            // Angle de rotation de la scene sur elle-meme autour de l'axe y
-static int anim = 0;               // Flag d'activation/desactivation de l'animation
-static int pMode = 1;              // Flag de switch entre modes d'affichage wireframe et fill 
 
-static int wTx = 480;              // Resolution horizontale de la fenetre
-static int wTy = 480;              // Resolution verticale de la fenetre
-static int wPx = 50;               // Position horizontale de la fenetre
-static int wPy = 50;               // Position verticale de la fenetre
-
-/* Fonction d'initialisation des parametres     */
-/* OpenGL ne changeant pas au cours de la vie   */
-/* du programme                                 */
-
+/* Init function */
 static void init(void) {
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
@@ -36,26 +25,19 @@ static void init(void) {
 	glEnable(GL_NORMALIZE);
 }
 
-/* Scene dessinee                               */
-
+/* Scene function */
 static void scene(void) {
 	glPushMatrix();
 	glutSolidTorus(0.2, 0.5, 72, 180);
 	glPopMatrix();
 }
 
-/* Fonction executee lors d'un rafraichissement */
-/* de la fenetre de dessin                      */
-
+/* Display function */
 static void display(void) {
 	printf("D\n");
 	glPolygonMode(GL_FRONT_AND_BACK, (pMode == 1) ? GL_FILL : GL_LINE);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glPushMatrix();
-	glRotatef(rx, 1.0F, 0.0F, 0.0F);
-	glRotatef(ry, 0.0F, 1.0F, 0.0F);
 	scene();
-	glPopMatrix();
 	glFlush();
 	glutSwapBuffers();
 	int error = glGetError();
@@ -63,13 +45,9 @@ static void display(void) {
 		printf("Attention erreur %d\n", error);
 }
 
-/* Fonction executee lors d'un changement       */
-/* de la taille de la fenetre OpenGL            */
-
+/* Reshape function */
 static void reshape(int wx, int wy) {
 	printf("R\n");
-	wTx = wx;
-	wTy = wy;
 	glViewport(0, 0, wx, wy);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -82,94 +60,58 @@ static void reshape(int wx, int wy) {
 	glLoadIdentity();
 }
 
-/* Fonction executee lorsqu'aucun evenement     */
-/* n'est en file d'attente                      */
-
+/* Idle function */
 static void idle(void) {
-	rx += sens;
 	glutPostRedisplay();
 }
 
-/* Fonction executee lors de l'appui            */
-/* d'une touche alphanumerique du clavier       */
-
+/* Keyboard function */
 static void keyboard(unsigned char key, int x, int y) {
 	switch (key) {
-	case 0x0D:
-	{ anim = !anim;
-	glutIdleFunc((anim) ? idle : NULL); }
-	break;
-	case ' ':
-	{ pMode = !pMode;
-	glutPostRedisplay(); }
-	break;
-	case 'f':
-	case 'F':
-		glutFullScreen();
+		case ' ':
+			{ pMode = !pMode;
+			glutPostRedisplay(); }
 		break;
-	case 0x1B:
-		exit(0);
-		break;
-	}
+		case 'f':
+		case 'F':
+			glutFullScreen();
+			break;
+		case 0x1B:
+			exit(0);
+			break;
+		}
 }
 
-/* Fonction executee lors de l'appui            */
-/* d'une touche speciale du clavier :           */
-/*   - touches de curseur                       */
-/*   - touches de fonction                      */
-
+/* Special function*/
 static void special(int specialKey, int x, int y) {
-	switch (specialKey) {
-	case GLUT_KEY_RIGHT:
-		ry += 1.0F;
-		glutPostRedisplay();
-		break;
-	case GLUT_KEY_LEFT:
-		ry -= 1.0F;
-		glutPostRedisplay();
-		break;
-	}
 }
 
-/* Fonction executee lors de l'utilisation      */
-/* de la souris sur la fenetre                  */
-
+/* Mouse function */
 static void mouse(int buton, int state, int x, int y) {
-	if ((buton == GLUT_RIGHT_BUTTON) && (state == GLUT_UP))
-		sens *= -1.0F;
 }
 
-/* Fonction executee lors du passage            */
-/* de la souris sur la fenetre                  */
-/* avec un boutton presse                       */
-
+/* Mouse function with pressed button */
 static void mouseMotion(int x, int y) {
 }
 
-/* Fonction executee lors du passage            */
-/* de la souris sur la fenetre                  */
-/* sans boutton presse                          */
-
+/* Mouse function without pressed button */
 static void passiveMouseMotion(int x, int y) {
 }
 
-/* Fonction exécutée automatiquement            */
-/* lors de l'exécution de la fonction exit()    */
-
+/* Called on exit */
 static void clean(void) {
 	printf("Bye\n");
 }
 
-/* Fonction principale                          */
-
+/* Main */
 int main(int argc, char **argv) {
 
 	atexit(clean);
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-	glutInitWindowSize(wTx, wTy);
-	glutInitWindowPosition(wPx, wPy);
+	glutInitWindowSize(400.0, 400.0);
+	glutInitWindowPosition(100.0, 100.0);
 	glutCreateWindow("Gestion événementielle de GLUt");
 	init();
 	glutKeyboardFunc(keyboard);
