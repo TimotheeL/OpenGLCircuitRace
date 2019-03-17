53f35bbab1f3a46851d240ab60c9afbde6880ed1
Unknown <timtim300@gmail.com>
2019-03-17 15:17:12 +0100

    Added car prototype project

---
+++ Include/Grid.h
@@ -0,0 +1,24 @@
+﻿/*
+	Racing game project - Grid class
+
+	2019
+	Nicolas Bouchard, Timoth�e Guy, Timoth�e Laurent
+*/
+
+#ifndef GRID_H
+#define GRID_H
+
+class Grid {
+
+public:
+	/* Constructor */
+	Grid(void);
+
+	/* Destructor */
+	~Grid(void);
+
+	/* Draw */
+	void draw(void);
+};
+
+#endif
---
+++ Include/RacingCar.h
@@ -0,0 +1,60 @@
+﻿/*
+	Racing game project - Racing car class
+
+	2019
+	Nicolas Bouchard, Timoth�e Guy, Timoth�e Laurent
+*/
+
+#ifndef RACINGCAR_H
+#define RACINGCAR_H
+
+class RacingCar {
+
+public:
+	static const float MAX_SPD_FW;	// Maximum speed forward
+	static const float MAX_SPD_BW;	// Maximum speed backward
+	static const float ACCEL;		// Acceleration
+	static const float ROT_ANGLE;	// Rotation angle
+
+	float x, y, z;		// Current position
+	float rotation;		// Current rotation
+	float speed;		// Current speed
+	bool b_handbrake;	// Handbrake state
+	bool dirForward;	// Direction state
+
+public:
+	/* Constructor */
+	RacingCar(void);
+
+	/* Destructor */
+	~RacingCar(void);
+
+	/*Draw */
+	void draw(void);
+
+	/* Input handling */
+	void handleInputs(bool *keyStates, bool *specialKeyStates);
+
+	/* Movement handling */
+	void handleMovement(double deltaTime);
+
+	/* Forward */
+	void forward(float distance);
+
+	/* Backward */
+	void backward(float distance);
+
+	/* Turn right */
+	void turnRight(float degrees);
+
+	/* Turn left */
+	void turnLeft(float degrees);
+
+	/* Use the handbrake */
+	void handbrake();
+
+	/* Reset car's properties */
+	void reset();
+};
+
+#endif
