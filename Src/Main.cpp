8d9e79ca18e0d2a4efb3e2226d93bd3ab9c1d979
Nicolas Bouchard <nicolas.bouchard@icloud.com>
2019-04-02 17:23:08 +0200

    Started to build Besançon Racing Track

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -26,17 +26,29 @@ static int pMode = 1;
 
 Patch patch = new Patch(-16.0, 16.0, 32.0, 5);
 
-static int wTx = 600;
-static int wTy = 360;
+static int wTx = 800;
+static int wTy = 600;
 static int wPx = 50;
 static int wPy = 50;
+
+static float eye_x = 0.0;
+static float eye_y = 180.0;
+static float eye_z = 1.0;
 
 std::vector<StraightLine> lines;
 std::vector<Turn> turns;
 
 /* Besançon Racing Track modelizing function */
 static void BRT(void) {
-	
+	lines.push_back(new StraightLine(7.0, 200.0, new Position(100.0, 0.0, 80.0, 90.0)));
+	turns.push_back(new Turn(7.0, 13.0, 192.0, false, new Position(-100.0, 0.0, 80.0, 90.0)));
+	lines.push_back(new StraightLine(7.0, 22.0, new Position(-96.8, 0.0, 47.3, 258.0)));
+	turns.push_back(new Turn(7.0, 6.0, 170.0, true, new Position(-75.5, 0.0, 51.8, 258.0)));
+	lines.push_back(new StraightLine(7.0, 42.0, new Position(-69.9 , 0.0, 33.7, 68.0)));
+	turns.push_back(new Turn(7.0, 10.0, 115.0, false, new Position(-106.5, 0.0, 19.0, 68.0)));
+	lines.push_back(new StraightLine(7.0, 20.0, new Position(-111.0, 0.0, -3.0, 313.0)));
+	turns.push_back(new Turn(7.0, 5.0, 90.0, false, new Position(-97.0, 0.0, -16.0, 313.0)));
+	lines.push_back(new StraightLine(7.0, 45.0, new Position(-85.1, 0.0, -15.7, 223.0)));
 }
 
 /* Init function */
@@ -94,7 +106,7 @@ static void display(void) {
 	}
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glPushMatrix();
-	gluLookAt(0.0, 200.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	gluLookAt(eye_x, eye_y, eye_z, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	scene();
 	glPopMatrix();
 	glFlush();
@@ -138,6 +150,12 @@ static void keyboard(unsigned char key, int x, int y) {
 			break;
 		case 0x1B:
 			exit(0);
+			break;
+		case 'p':
+			eye_y += 10.0;
+			break;
+		case 'm':
+			eye_y -= 10.0;
 			break;
 		}
 }
